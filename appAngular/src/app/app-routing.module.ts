fc7c20e701d0d7a5f147428665f7ccc0d492bf97
Leonardo Braz <leonardo@uitecnologia.com.br>
2022-12-16 17:35:56 -0300

    Tela de login e home

--- appAngular/src/app/app-routing.module.ts
+++ appAngular/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+
+import { TelaLoginComponent } from 'src/app/page/tela-login/tela-login.component';
+import { HomeComponent } from 'src/app/page/home/home.component';
+
+const routes: Routes = [
+    {
+      path: '',
+    redirectTo: 'tela-login',
+    pathMatch: 'full'
+  },
+    {path: 'tela-login', component: TelaLoginComponent},
+    {path: 'home', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- appAngular/src/app/app.module.ts
+++ appAngular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TelaLoginComponent } from './page/tela-login/tela-login.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaLoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
